36fe76ba186f7ab04c2a53cb85f98ffbad775866
Samodra <szsamodra@gmail.com>
2020-04-29 23:26:34 +0700

    Initial commit

---
+++ PRG2_20192_P1_3_26/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace PRG2_20192_P1_3_26
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("PROGRAM ANAK AYAM");
+            Console.WriteLine("-----------------");
+            Console.Write("Masukkan nilai N : ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("ANAK AYAM TURUN {0}\n", n);
+
+            for(int i = n; i >= 1; i--)
+            {
+                Console.Write("Anak ayam turun {0}", i);
+                if((i - 1) == 0)
+                {
+                    Console.WriteLine(", mati satu tinggal induknya");
+                } else
+                {
+                    Console.WriteLine(", mati satu tinggal {0}", (i - 1));
+                }
+            }
+        }
+    }
+}
